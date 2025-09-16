26fe9036d5ed965bfa6a5d619d1580b51060a2e8
Carlos030756 <31417816+Carlos030756@users.noreply.github.com>
2025-09-16 20:31:23 +0200

    Delete card-scanner-web directory

--- card-scanner-web/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('csw-v1').then(c => c.addAll(['/','/index.html','/script.js','/manifest.json'])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
-  );
-});
